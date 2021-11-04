e1c55a080a54bfa28cda74a3365dc1d4e7f1ab39
mdqueiroz1 <mdqueiroz12@gmail.com>
2021-11-04 17:43:59 -0300

    mudança de nome do projeto

--- conversorAngular/karma.conf.js
+++ conversorAngular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/conversor-alunos'),
+      dir: require('path').join(__dirname, './coverage/conversor-Angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
